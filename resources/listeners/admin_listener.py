4876b61a0481d33b240d7648a4c5f03d77e3a369
ayaka_shimizu <a_shimizu@dsr.co.jp>
2026-01-28 16:30:37 +0900

    Listインポートエラー

--- resources/listeners/admin_listener.py
+++ resources/listeners/admin_listener.py
@@ -14,6 +14,7 @@ import json
 import logging
 import re
 import datetime
+from typing import List, Dict, Any
 
 from resources.listeners.Listener import Listener
 from resources.services.group_service import GroupService
